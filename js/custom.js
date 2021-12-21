80a931feecb8326b55731b4b73460ee925cc045c
jruiz <javiernj1@gmail.com>
2021-12-21 19:26:49 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('#color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    });
+
+    // FIELD FOCUS
+    $('.form-control').focus(function(){
+        $(this).parent().addClass('is-focused')
+    });
+})(jQuery);
